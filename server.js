a07f6680dcc13c840388adbb9890160d8b9340ee
salybu <rkdmsdl93@gmail.com>
2022-05-05 12:53:47 +0900

    feat: reacp GraphQL (#3.3 POC API)

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    // createMovie: (root, args, context, info) => "",
+    createMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
